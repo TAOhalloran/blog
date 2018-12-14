8398656c1519b4aa88e0ee3adbf906a31c373ccc
tomohalloran@outlook.com <tomohalloran@outlook.com>
2018-12-14 21:50:49 +0000

    styling

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,23 @@
+class PagesController < ApplicationController
+
+def home
+
+end
+
+def about
+
+end
+
+def links
+  
+end
+
+def faq
+  
+end
+
+def contact
+
+end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+
   resources :articles
+  
+  root 'pages#home'
+    
+  get 'pages/about', to: 'pages#about'
+
+  get 'pages/faq', to: 'pages#faq'
+
+  get 'pages/links', to: 'pages#links'
+
+  get 'pages/contact', to: 'pages#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
