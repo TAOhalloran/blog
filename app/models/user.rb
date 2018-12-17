4fa2dbbd9127d376e8026650a8a78790468103d4
tomohalloran@outlook.com <tomohalloran@outlook.com>
2018-12-17 16:35:05 +0000

    add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: { case_sensative: false }, 
+  length: { minimium: 3, maximium: 25 }
+  
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  
+  validates :email, presence: true, uniqueness: { case_sensative: false }, 
+  format: { with: VALID_EMAIL_REGEX }
   
 end
