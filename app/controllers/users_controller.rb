e42c1efab41bd63b5fdcedf3e66bfb7fded07a09
tomohalloran@outlook.com <tomohalloran@outlook.com>
2018-12-17 19:53:09 +0000

    added show user page, and gravaton

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,13 @@
 class UsersController < ApplicationController
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
   def new
     @user = User.new
   end
+  
   def create
     @user = User.new(user_params)
     if @user.save
@@ -25,6 +31,8 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+  
+
   
   private
   def user_params
